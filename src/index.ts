c0e712546a792962bcb5dd6996248c5e948d6268
albmar <ak.albmar@gmail.com>
2026-04-10 23:08:53 +0200

    Add CSV class

--- src/index.ts
+++ src/index.ts
@@ -151,6 +151,71 @@ class ReloadState {
   gachaResults: Map<ItemId, GachaResult> = new Map();
 }
 
+class CSV {
+  header: string[] = [];
+  data: any[] = [];
+
+  addColumn(name: string) {
+    this.header.push(name);
+    return this;
+  }
+
+  addCell(cell: any) {
+    this.data.push(cell);
+    return this;
+  }
+
+  escape(value: string): string {
+    const safe = value.replace(/"/g, '""');
+    return `"${safe}"`;
+  }
+
+  unescape(value: string): string {
+    let match = value.match(/^"(.*)"$/);
+    if (!match || !match[1])
+      throw new Error(`The value has to be in quotes, but was: ${value}`);
+    return match[1].replace(/""/g, '"');
+  }
+
+  import(outputPath: string, seperator: string = ",") {
+    const targetPath = outputPath
+      ? path.resolve(__dirname, outputPath)
+      : path.resolve(__dirname, "data.csv");
+
+    const csv = fs.readFileSync(targetPath, { encoding: "utf8" });
+    const lines = csv
+      .split("\n")
+      .map((line) => line.split(seperator))
+      .values();
+    this.header = lines.next().value!;
+    this.data = [...lines].map((line) =>
+      line.map((v) => (v[0] == '"' ? this.unescape(v) : Number.parseFloat(v))),
+    );
+  }
+
+  export(outputPath: string, seperator: string = ",") {
+    const targetPath = outputPath
+      ? path.resolve(__dirname, outputPath)
+      : path.resolve(__dirname, "data.csv");
+
+    const numCol = this.header.length;
+    let lines = [this.header.join(seperator)];
+    let i = 0;
+    while (i * numCol < this.data.length) {
+      lines.push(
+        this.data
+          .slice(i * numCol, (i + 1) * numCol)
+          .map((v) => (typeof v == "string" ? this.escape(v) : v))
+          .join(seperator),
+      );
+      i++;
+    }
+
+    const csv = lines.join("\n");
+    fs.writeFileSync(targetPath, csv, { encoding: "utf8" });
+  }
+}
+
 type RecipeId = number;
 
 type ItemId = number;
